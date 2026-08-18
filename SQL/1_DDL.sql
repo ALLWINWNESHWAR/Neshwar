10b567bfd583dd03c82e8621a22c7ca0e1501295
ALLWIN W NESHWAR V <allwinwneshwar@gmail.com>
2026-08-18 18:16:44 +0530

    Update customer records in 1_DDL.sql

--- SQL/1_DDL.sql
+++ SQL/1_DDL.sql
@@ -9,4 +9,7 @@ create table customer(customer_id number(6),
 
 select * from customer;
 insert into customer (customer_id,cust_name,mobile_no,dob,city,mail)
-values (2203,'allwin',9626932842,to_date('18/08/2026', 'dd/mm/yyyy'),'chennai','allwin@gmail.com')
+values (2203,'allwin',9626932842,to_date('28/04/2003', 'dd/mm/yyyy'),'chennai','allwin@gmail.com')
+
+insert into customer
+values (2201,'akilan',9999966666,to_date('15/01/2025', 'dd/mm/yyyy'),'salem','akilan@gmail.com')
