9ccd9c4017edd3ad1f1d6ddb85c4fc68794766d4
ALLWIN W NESHWAR V <allwinwneshwar@gmail.com>
2026-08-18 21:43:50 +0530

    Enhance DDL script with comments and table modifications
    Added comments for DDL operations and modified customer table structure with updates and inserts.

--- SQL/1_DDL.sql
+++ SQL/1_DDL.sql
@@ -1,4 +1,9 @@
 /* DDL - Data Definition Language */
+/* 1. create */
+/* 2. insert*/
+/* 3. update */
+/* 4. alter */
+/* 5. drop */
 create table customer(customer_id number(6),
   cust_name number(10),
   mobile_no number(10),
@@ -13,3 +18,30 @@ values (2203,'allwin',9626932842,to_date('28/04/2003', 'dd/mm/yyyy'),'chennai','
 
 insert into customer
 values (2201,'akilan',9999966666,to_date('15/01/2025', 'dd/mm/yyyy'),'salem','akilan@gmail.com')
+
+insert into customer
+values (2205,null,1234567890,to_date('23/05/2024', 'dd/mm/yyyy'),'thiruvarur','samsa@gmail.com')
+
+select * from customer;
+
+rollback;        /* it mean : ctrl+z */
+
+commit;          /* it mean : it is commited */
+
+/* update */
+update customer
+set cust_name='samsa', where customer_id=2205;
+
+/* add a coloum */
+alter table customer
+add country varchar2(50);
+
+update customer set country='india';
+
+/* drop a coloum */
+alter table customer
+drop coulumn city;
+
+
+
+
