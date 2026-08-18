482e5dc828ae91996ff91405a6557b9660a887e2
ALLWIN W NESHWAR V <allwinwneshwar@gmail.com>
2026-08-18 18:09:33 +0530

    Create customer table and insert sample data

--- SQL/1_DDL.sql
+++ SQL/1_DDL.sql
@@ -1,2 +1,12 @@
 /* DDL - Data Definition Language */
+create table customer(customer_id number(6),
+  cust_name number(10),
+  mobile_no number(10),
+  dob date,
+  city varchar2(30),
+  mail varchar2(30)
+  );
 
+select * from customer;
+insert into customer (customer_id,cust_name,mobile_no,dob,city,mail)
+values (2203,'allwin',9626932842,to_date('18/08/2026', 'dd/mm/yyyy'),'chennai','allwin@gmail.com')
