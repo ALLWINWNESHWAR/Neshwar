3e49e94b76199c9902123f383746b3fcfde7a03b
ALLWIN W NESHWAR V <allwinwneshwar@gmail.com>
2026-08-19 21:37:06 +0530

    Create and modify student and employee tables
    Created tables for students and employees, added columns to student table, modified department column, renamed student_name to full_name, and dropped phone column.

--- SQL/2_DDL_practice.sql
+++ SQL/2_DDL_practice.sql
@@ -1,1 +1,42 @@
+/* create */
+create table student(student_id number(6),
+  student_name varchar2(50),
+  age number(5),
+  email varchar2(100),
+  department varchar2(30)
+  );
+
+create table employee (emp_id number(6),
+  emp_name varchar2(50),
+  salary number(10,2),
+  job_role varchar2(40),
+  joining_date date,
+  phone varchar2(15)
+  );
+
+/* alter */
+alter table student
+add city varchar2(30);
+
+alter table student
+add (
+  gender varchar2(10),
+  marks number(5,2)
+  );
+
+/* modify */
+alter table student
+modify department varchar2(50);
+
+/* rename */
+alter table student
+rename column student_name to full_name;
+
+/* drop */
+alter table student
+drop column phone;
+
+drop table employee;
+
+
 
