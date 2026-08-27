ec662b6a52d6ff33302d56d6bfa1bf4ced3ae4f7
ALLWIN W NESHWAR V <allwinwneshwar@gmail.com>
2026-08-27 23:58:12 +0530

    Modify customer data and enhance select queries
    Updated customer entries with new age values and added various select statements for data retrieval.

--- SQL/3_Constraints.sql
+++ SQL/3_Constraints.sql
@@ -26,17 +26,35 @@ create table customer(
   );
 insert all
 into customer values(2201,'allwin',1234567890, 23,30),
-into customer values(2202,'gopal',0987654321, 23,30),
-into customer values(2203,'balaji',1234509876, 23,30),
-into customer values(2204,'madesh',0987612345, 23,30),
-into customer values(2205,'ragul',6789012345, 23,30),
+into customer values(2202,'gopal',0987654321, null,30),
+into customer values(2203,'balaji',1234509876, 21,30),
+into customer values(2204,'madesh',0987612345, 19,30),
+into customer values(2205,'ragul',6789012345, null,30),
 into customer values(2206,'karthick',5432167890, 23,30),
-into customer values(2207,'zee',5432109876, 23,30),
-into customer values(2208,'meg',6789054321, 23,30),
-into customer values(2209,'begam',2345678910, 23,30),
-into customer values(2210,'barani',2345678901, 23,30),
+into customer values(2207,'zee',5432109876, 19,30),
+into customer values(2208,'meg',6789054321, null,30),
+into customer values(2209,'begam',2345678910, 20,30),
+into customer values(2210,'barani',2345678901, 18,30),
 commit;
 
 --select statement 
+select * from city;
+select * from customer;
 
-  
+-- column alias
+select cus_id,cus_name as Name from customer;
+
+--column concatination
+select cus_id || ' ' || cus_name as Name, mobile from customer;
+
+--column calculation
+select cus_name, age, age+10 as after_10_years from customer;
+
+--unique
+select distinct age from customer;
+
+--count
+select count(distinct age from customer;
+
+-- where clause
+select cus_name , age from 
